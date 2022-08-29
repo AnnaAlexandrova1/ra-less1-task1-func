57d33a5b6ae5cdb70c89f05c781a84eb89426365
Anna <anna4776497@gmail.com>
2022-08-29 19:09:12 +0300

    готово

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ShopItemFunc from './ShopItemFunc'
 import Item from './Item';
 
 const item = new Item({
@@ -12,22 +11,6 @@ const item = new Item({
 })
 
 function App() {
- /* const item = [{
-      brand: 'Tiger of Sweden',
-      title: 'Leonard coat',
-      description: 'Minimalistic coat in cotton-blend',
-      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-      price: 399,
-      currency: '£'
-    }]*/
-  
-
-   /* const elements = items.map((i) => {
-          return (
-            <ShopItemFunc brand={i.brand} title={i.title} description={i.description}/>
-          )
-    })*/
-
   return (
     <div className="container">
       <div className="background-element">
